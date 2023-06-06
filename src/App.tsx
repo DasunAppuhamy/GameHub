2e4f30dbeafe45630033b1ac8d8844ee1facbf7d
DasunAppuhamy <milindampn@gmail.com>
2023-06-06 19:03:08 +0100

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="teal" variant="outline">
-        Button
-      </Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          NAV
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            ASIDE
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          MAIN
+        </GridItem>
+      </Grid>
     </>
   );
 }
