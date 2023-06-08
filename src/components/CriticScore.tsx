b2c6d8794d3b600896b3dd6ad3a94678d2d584df
DasunAppuhamy <milindampn@gmail.com>
2023-06-08 19:13:34 +0100

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,24 @@
+import { Badge, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+
+interface Props {
+  game: Game;
+}
+const CriticScore = ({ game }: Props) => {
+  let color =
+    game.metacritic > 75 ? "green" : game.metacritic > 60 ? "yellow" : "red";
+  return (
+    <>
+      <Badge
+        fontSize="14px"
+        borderRadius="4px"
+        paddingX={2}
+        colorScheme={color}
+      >
+        {game.metacritic}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
